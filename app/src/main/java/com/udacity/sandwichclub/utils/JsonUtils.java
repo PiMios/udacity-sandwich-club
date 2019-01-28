a621efe649f83f1b5974f3525e32cb81b9c89d14
monstorbak <nick@pimios.com>
2019-01-28 08:51:29 -0500

    Import sandwich club starter code

---
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,15 @@
+package com.udacity.sandwichclub.utils;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        String mainName;
+        String placeOfOrigin;
+        String description;
+        String image;
+
+        return null;
+    }
+}
