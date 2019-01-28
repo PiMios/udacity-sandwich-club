75ff8b0e7d12f806e5441565376b6be836db7250
monstorbak <nick@pimios.com>
2019-01-28 10:05:36 -0500

    Not Loading
    Crashes loader on emulator.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +59,16 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView ingredients = (TextView) findViewById(R.id.ingredients_tv);
+        TextView ingredientsLabel = (TextView) findViewById(R.id.tv_ingredients_label);
+        TextView alsoKnownAs = (TextView) findViewById(R.id.also_known_tv);
+        TextView akaLabel = (TextView) findViewById(R.id.tv_aka_label);
+        TextView alsoKnown = (TextView) findViewById(R.id.also_known_tv);
+        TextView originLabel = (TextView) findViewById(R.id.tv_place_of_origin);
+        TextView origin = (TextView) findViewById(R.id.origin_tv);
+        TextView descriptionLabel = (TextView) findViewById(R.id.description_tv);
+        ImageView image = (ImageView) findViewById(R.id.image_iv);
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,6 +2,12 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
@@ -9,6 +15,33 @@ public class JsonUtils {
         String placeOfOrigin;
         String description;
         String image;
+
+        try {
+            JSONObject sammichDetails = new JSONObject(json);
+            JSONObject name = sammichDetails.getJSONObject("name");
+            mainName = name.getString("manName");
+
+            JSONArray alsoKnwonAsArray = name.getJSONArray("alsoKnownAs");
+            ArrayList<String> sammichNames = new ArrayList<>();
+            for (int i = 0; i < alsoKnwonAsArray.length(); i++) {
+                sammichNames.add(alsoKnwonAsArray.getString(i));
+            }
+
+            placeOfOrigin = sammichDetails.getString("placeOfOrigin");
+            description = sammichDetails.getString("description");
+
+            image = sammichDetails.getString("image");
+
+            JSONArray ingredientsArray = sammichDetails.getJSONArray("ingredients");
+            ArrayList<String> sammichIngredients = new ArrayList<>();
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                sammichIngredients.add(ingredientsArray.getString(i));
+            }
+
+            return new Sandwich(mainName, sammichNames, placeOfOrigin, description, image, sammichIngredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
